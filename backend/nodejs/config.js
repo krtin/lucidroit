4ace60745b840065376ea2bd2559063ebef5992e
Krtin Kumar <krtinkumar@gmail.com>
2019-03-12 21:33:12 -0400

    deleted files

--- backend/nodejs/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/lucidroit',
-    'facebook': {
-        clientId: '133629407351285',
-        clientSecret: '11167122026abfc7c1205d185c560d5a'
-    }
-}
